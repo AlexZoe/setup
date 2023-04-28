455cf04393946f5400a20def2006d88e96716ab9
Alexander Zoellner <zoellner.contact@gmail.com>
2023-04-28 13:49:52 +0900

    [fix] Rust Ctags

--- texteditor/.vim/plugin/gen_tags.sh
+++ texteditor/.vim/plugin/gen_tags.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-SRC_LOC="./"
+SCRIPT_DIR="$(dirname "$(realpath "$0")")"
+WORK_DIR=$(pwd)
+
 declare -a LANGUAGES=( "c" "c++" "Rust" )
 FILE_TYPE=".*\.(c|cc|cpp|cu|h|hpp|cuh|rs)"
-
-# Terminate on error
-set -e
+RUST_TAGS="${SCRIPT_DIR}/ctags.rust"
 
 # Create cscope
-rm -rf ${SRC_LOC}/cscope.files
-find -L ${SRC_LOC} -regextype egrep -regex "$FILE_TYPE" \
+rm -rf ${WORK_DIR}/cscope.files
+find -L ${WORK_DIR} -regextype egrep -regex "$FILE_TYPE" \
     | grep -vE "build|component|external" > cscope.files
 cscope -b
 
-rm -rf ${SRC_LOC}/tags
+rm -rf ${WORK_DIR}/tags
 # Create ctags
 tmp=""
 for i in "${LANGUAGES[@]}"
@@ -25,6 +25,5 @@ do
     fi
 done
 
-#ctags -R ${SRC_LOC} --languages=${tmp}
-ctags --langmap=C++:+cu --langmap=C++:+.cuh --languages=${tmp} \
+ctags --options="${RUST_TAGS}" --langmap=C++:+.cu --langmap=C++:+.cuh --languages=${tmp} \
     --exclude=build --exclude=components --exclude=external -L cscope.files
