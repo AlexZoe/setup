76d56bc7724f7260efd4947cc2f440b0dddf8546
Alexander Zoellner <zoellner.contact@gmail.com>
2024-11-26 03:55:46 +0900

    [tmux] Spawn Session Script

--- tmux/setup_session.sh
+++ tmux/setup_session.sh
@@ -1,11 +1,41 @@
-#!/bin/zsh
+#!/bin/sh
 
-SESSIONNAME="main"
-tmux has-session -t $SESSIONNAME &> /dev/null
+while [ $# -gt 0 ]; do
+    key="$1"
+
+    case $key in
+        -s|--session)
+            SESSIONNAME=$2
+            shift
+            shift
+            ;;
+        -a|--attach)
+            ATTACH=true
+            shift
+            ;;
+        -h|--help)
+            PRINT_HELP=1
+            shift
+            ;;
+        *)    # unknown option
+            shift
+            ;;
+    esac
+done
+
+if [ ! $SESSIONNAME ]; then
+    echo "no session name"
+    exit 1
+fi
+
+tmux has-session -t $SESSIONNAME > /dev/null 2>&1
 
 if [ $? != 0 ]; then
     tmux new-session -s $SESSIONNAME -n editor -d
     tmux new-window -n build
+    tmux new-window -n docs
 fi
 
-tmux attach -t $SESSIONNAME
+if [ -n "$ATTACH" ]; then
+    tmux a
+fi
